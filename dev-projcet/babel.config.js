cfa7e6df2e04876d6bf9a3c7ce3e6dba5c437f31
devilwjp <9700616@qq.com>
2022-02-06 01:25:55 +0800

    +++

--- dev-projcet/babel.config.js
+++ dev-projcet/babel.config.js
@@ -1,5 +1,33 @@
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname,  dir)
+}
+
 module.exports = {
   presets: [
+    // vue的plugin包
     '@vue/cli-plugin-babel/preset'
+  ],
+  // 使用overrides是为了分开配置jsx的解析规则，在react_app里使用react的规则解析，其他使用vue的规则
+  overrides: [
+    {
+      // 这里如果不用函数，会出现报错的情况，还没找到原因
+      // babel会提示Configuration contains string/RegExp pattern, but no filename was passed to Babel
+      // 因为filename不存在，如果不存在就必须通过回调返回一个undefined
+      test:function(filename) {
+        // 仅对src/react_app目录有效
+        if (filename !== undefined && filename.indexOf(resolve('src/react_app')) === 0) return filename
+      },
+      plugins: [
+        // 使用react的jsx编译
+        'transform-react-jsx'
+      ],
+      presets: [
+        ['@vue/cli-plugin-babel/preset', {
+          // 关闭vue的jsx编译
+          jsx: false
+        }]
+      ]
+    }
   ]
 }
--- dev-projcet/vue.config.js
+++ dev-projcet/vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  devServer: {
+    overlay: {
+      warnings: false,
+      errors: false
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        combined: path.resolve(__dirname, '../src'),
+        src: path.resolve(__dirname, './src'),
+        react_app: path.resolve(__dirname, './src/react_app'),
+      }
+    }
+  }
 }
