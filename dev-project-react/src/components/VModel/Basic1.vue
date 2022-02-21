52875531c1560ec2da466c13199857fb06aa2c59
devilwjp <9700616@qq.com>
2022-02-22 02:32:32 +0800

    ++++

---
+++ dev-project-react/src/components/VModel/Basic1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="vue-component">
+    <h3>This is the Vue Component.</h3>
+    received modelValue's value: {{$attrs.modelValue}}<br/>
+    received bar's value: {{$attrs.bar}}
+    <slot/>
+  </div>
+</template>
+<script>
+import {onMounted, getCurrentInstance, onUnmounted} from 'vue'
+export default {
+  setup() {
+    let timer
+    const instance = getCurrentInstance()
+    onMounted(() => {
+      timer = setInterval(() => {
+        instance.emit('update:modelValue', Math.random())
+        instance.emit('update:bar', Math.random())
+      }, 1000)
+    })
+    onUnmounted(() => {
+      clearInterval(timer)
+    })
+  }
+}
+</script>
+<style scoped>
+
+</style>
