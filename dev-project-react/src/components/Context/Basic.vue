51e66ab0fdeefc0655a7be5ca65d04528c29269b
devilwjp <9700616@qq.com>
2022-02-22 23:28:10 +0800

    ++++

---
+++ dev-project-react/src/components/Context/Basic.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vue-component">
+    <h3>This is the Vue Component.</h3>
+    received foo's value: {{$attrs.foo}}
+    <slot/>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
