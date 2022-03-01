06c715c9e68c2fa89e69081516bd9e733f4e4804
devilwjp <9700616@qq.com>
2022-03-01 14:18:15 +0800

    🪂

--- dev-project-vue3/babel.config.js
+++ dev-project-vue3/babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '@vue/cli-plugin-babel/preset',
     ['veaury/babel/preset', {
       mode: 'reactInVue',
+      // for dev only
       // test: function(filename) {
       //   // The files in the following paths are compiled with React's jsx
       //   if (filename?.startsWith(resolve('src')) && filename.match(/[/\\]react_app[\\/$]+/) || filename?.startsWith(resolve('../src'))) return filename
