410bf1d8d6dc944398b64148d9595b64e3a8cd00
devilwjp <9700616@qq.com>
2022-05-30 02:01:34 +0800

    😤😢🤯😩

--- src/injectPropsFromWrapper.js
+++ src/injectPropsFromWrapper.js
@@ -1,4 +1,5 @@
 export default function(injectionHook, Component) {
+  console.warn(`[veaury warn]: HOC injectPropsFromWrapper is deprecated! Try using 'useInjectPropsFromWrapper' in the options of 'applyReactInVue' or 'applyVueInReact'!`)
   if (typeof injectionHook !== 'function') {
     console.warn(`[veaury warn]: parameter 'injectionHook' is not a function`)
     return Component
