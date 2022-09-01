23cc1ff2ecace7d386920cf7944726ec75955446
devilwjp <9700616@qq.com>
2022-09-01 17:49:37 +0800

    🐕‍🦺🐩🐕🐈🐈‍⬛🐅

---
+++ tests/cases/applyReactInVue/LazyReactInVue.vue
@@ -0,0 +1,10 @@
+<template>
+  <AA/>
+</template>
+
+<script setup>
+import * as Vue from 'vue'
+import {applyReactInVue} from 'veaury'
+import AAReact from './AA';
+const AA = applyReactInVue(AAReact)
+</script>
