8588040da55b88b9c227cfebcc4b19ae7e437df6
devilwjp <9700616@qq.com>
2022-09-05 02:15:06 +0800

    😈👿👹👺💀☠️

---
+++ dev-project-react/src/components/CrossProvider/BasicPure.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="vue-component">
+    <h3>This is the Vue Component.</h3>
+    the path info from 'react-router': <span style="font-weight: bold" data-testid="pathnameSearch">{{pathname + search}}</span><br/><br/>
+    <button @click="changeQuery" data-testid="changeQuery">change query</button>
+  </div>
+</template>
+<script>
+import { useReactRouterForVue } from './reactRouterCrossingProvider'
+import React from 'react'
+
+export default {
+  setup() {
+    const { location, navigate } = useReactRouterForVue()
+    function changeQuery() {
+      navigate(`?a=${Math.random()}`, {replace: true})
+    }
+    return {
+      pathname: location.pathname,
+      search: location.search,
+      changeQuery
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
