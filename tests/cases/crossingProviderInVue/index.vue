edd6a0aa4b56620645be182dd62fca4eff2c352e
devilwjp <9700616@qq.com>
2022-09-04 23:34:22 +0800

    🗻🌋⛰️🏔️🧭🚤

---
+++ tests/cases/crossingProviderInVue/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3>
+    This example shows the basic usage of `createCrossingProviderForPureReactInVue`.
+  </h3>
+  <h4>
+    Using React components in Vue components.
+  </h4>
+  <ProviderInVue>
+    <Basic/>
+  </ProviderInVue>
+</template>
+
+<script>
+import { applyPureReactInVue } from 'veaury'
+// This is a React Component
+import ReactBasic from "./react_app/Basic"
+import {ProviderInVue} from "./vueRouterAndVuexCrossingProvider";
+
+export default {
+  components: {
+    ProviderInVue,
+    Basic: applyPureReactInVue(ReactBasic),
+  },
+  mounted() {
+    // console.log(11111, this.$refs.aaa)
+
+  }
+}
+</script>
+
+<style scoped>
+.slot {
+  background: aquamarine;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
