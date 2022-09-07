5526cb040f39b447e4133d8f759b831f1e359501
devilwjp <9700616@qq.com>
2022-09-08 01:57:22 +0800

    Add unit tests for babel preset 😇😇😇

--- tests/anyEmpty.js
+++ tests/anyEmpty.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// This is a custom Jest transformer turning style imports into empty objects.
-// http://facebook.github.io/jest/docs/en/webpack.html
-
 module.exports = {
   process() {
     return {
@@ -11,6 +8,6 @@ module.exports = {
   },
   getCacheKey() {
     // The output is always the same.
-    return 'cssTransform';
+    return 'anyEmpty';
   },
 };
