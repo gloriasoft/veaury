4fe7f0ad9ea93d879db79f8a285d6d88d2f551ea
devilwjp <9700616@qq.com>
2022-09-17 04:53:56 +0800

    chore(test): Improve test coverage🤡🤡🤡

--- tests/cases/vitePlugin/1-test.js
+++ tests/cases/vitePlugin/1-test.js
@@ -9,14 +9,14 @@ describe('Test babel/ReactPreset.js', () => {
     const result = vitePlugin({
       type: 'vue'
     })
-    expect(result.length === 4).toBe(true)
-    expect(!!result[2].config()).toBe(true)
+    expect(result.length === 6).toBe(true)
+    expect(!!result[4].config()).toBe(true)
   })
   test('Test type react', () => {
     const result = vitePlugin({
       type: 'react'
     })
-    expect(result.length === 4).toBe(true)
+    expect(result.length === 6).toBe(true)
   })
   test('Test type custom', () => {
     let result = vitePlugin({
@@ -25,11 +25,35 @@ describe('Test babel/ReactPreset.js', () => {
       vueJsxInclude: [/vue&type=script&lang\.[tj]sx?$/, /vue&type=script&setup=true&lang\.[tj]sx?$/, /[/\\]vue_app[\\/$]+/],
       vueJsxExclude: []
     })
-    expect(result.length === 4).toBe(true)
+    expect(result.length === 6).toBe(true)
 
     result = vitePlugin({
       type: 'custom'
     })
-    expect(result.length === 4).toBe(true)
+    expect(result.length === 6).toBe(true)
+  })
+  test('Test react-dom/client', async () => {
+    const result = vitePlugin({
+      type: 'vue'
+    })
+    const resolveId = result[0].resolveId
+    // fake this.resolve
+    let fakeThis = {
+      resolve: () => true
+    }
+    let resolution
+    resolution = await resolveId.call(fakeThis, 'react-dom/client', '')
+    expect(resolution).toBe(undefined)
+    resolution = await resolveId.call(fakeThis, 'aabbcc', '')
+    expect(resolution).toBe(undefined)
+    fakeThis = {
+      resolve: () => null
+    }
+    resolution = await resolveId.call(fakeThis, 'react-dom/client', '')
+    expect(resolution.id === 'veaury-fake-react-dom-client').toBe(true)
+
+    const load = result[0].load
+    expect(!!load('veaury-fake-react-dom-client')).toBe(true)
+    expect(!!load('asasas')).toBe(false)
   })
 })
