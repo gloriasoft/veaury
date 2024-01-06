d213ddde424f15416ea8788ff29829c1be6f1094
zhangHongEn <zhang_h_n@163.com>
2024-01-06 10:59:55 +0800

    add case

---
+++ tests/cases/applyReactInVue/4-reactUnMount.vue
@@ -0,0 +1,31 @@
+<template>
+  <ReactComponentInVue v-if="showFlag"/>
+</template>
+
+<script>
+import {h, ref, onMounted} from 'vue'
+import {applyReactInVue} from 'veaury'
+import ReactComponent from "./react_app/ReactUnMount"
+
+export default {
+    components: {
+        ReactComponentInVue: applyReactInVue(ReactComponent)
+    },
+    setup() {
+        const showFlag = ref(true)
+        onMounted(() => {
+            setTimeout(() => {
+                showFlag.value = false
+            }, 200);
+        })
+
+        return {
+            showFlag
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
