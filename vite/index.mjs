2a03ba98912803b5250469051a50805430548f96
Yuyuko233 <yuyuko233@para.party>
2024-05-21 15:18:20 +0800

    Create index.mjs

---
+++ vite/index.mjs
@@ -0,0 +1,44 @@
+import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+function veauryVitePlugins({type, vueJsxInclude, vueJsxExclude, vueOptions = {}, vueJsxOptions: initVueJsxOptions = {}, reactOptions = {}}) {
+
+  let vueJsxOptions = {...initVueJsxOptions}
+  if (type === 'react') {
+    vueJsxOptions.include = [/vue&type=script&lang\.[tj]sx$/i, /vue&type=script&setup=true&lang\.[tj]sx$/i, /[/\\]vue_app[\\/][\w\W]+\.[tj]sx$/]
+  }
+  if (type === 'vue') {
+    vueJsxOptions.exclude = [/[/\\]react_app[\\/$]+/]
+  }
+  if (type === 'custom') {
+    if (vueJsxInclude) {
+      vueJsxOptions.include = vueJsxInclude
+    }
+    if (vueJsxExclude) {
+      vueJsxOptions.exclude = vueJsxExclude
+    }
+  }
+
+  return [
+    vue(vueOptions),
+    // Make vueJsx plugin run time earlier
+    {
+      ...vueJsx(vueJsxOptions),
+      enforce: 'pre'
+    },
+    // recover esbuild include
+    {
+      config(){
+        return {
+          esbuild: {
+            include: /\.[jt]sx?$/
+          }
+        }
+      }
+    },
+    react(reactOptions)
+  ]
+}
+
+export default veauryVitePlugins
